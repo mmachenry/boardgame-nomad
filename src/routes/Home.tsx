d46c7c147a71e3094b7679ff03d9582a72429116
Mike MacHenry <mike.machenry@gmail.com>
2024-10-29 00:01:41 -0400

    add mui

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button'
+
 const Home = () => {
   return (
     <>
       Welcome to Boardgame Nomad!
+      <Button variant="contained" href="/categories">Categories</Button>
     </>
   )
 }
