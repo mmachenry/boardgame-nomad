4d877a149b0909f8b031a34f3a48b1dfe518a6fa
mmachenry <mike.machenry@gmail.com>
2024-10-31 00:59:45 -0400

    title test

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 const Home = () => {
   return (
     <>
-      <h1>Welcome to Boardgame Nomad!</h1>
+      <h1>Welcome to the Boardgame Nomad!</h1>
       <List>
         <ListItem component={Link} to='/categories'>Categories</ListItem>
         <ListItem component={Link} to='/the-crew'>The Crew</ListItem>
